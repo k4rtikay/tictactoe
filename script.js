d2583c03a55dd488e17ea08974e45016664519a1
k4rtikay <katt1bh4i@gmail.com>
2024-12-05 05:10:38 +0000

    Creates the basic gird and the start button.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function playerOne(){
+
+const container = document.querySelector('.container');
+
+/*function playerOne(){
     let uid;
     let arrP1 = [];
     function user (){
         uid = prompt("Enter Player One:");
-    } 
+    }
     return {setUser: user,
         get uid(){
             return uid;
@@ -17,28 +20,40 @@ function playerTwo(){
     let arrP2 = [];
     function user (){
         uid = prompt("Enter Player Two:");
-    } 
+    }
     return {setUser: user,
         get uid(){
             return uid;
         },
         arr: arrP2
     };
-}
+}*/
+
+
 
 
 const gameboard =(function () {
-    const container = document.querySelector('.boxContainer');
+    const boxContainer = document.querySelector('.boxContainer');
     function createBoard (){
-        for(i=0;i<9;i++){
-            boxContainer.appendChild(document.createElement(div).classList.add('box'));
+        for(let i=0;i<9;i++){
+            let newBox = document.createElement('div');
+            newBox.classList.add('box');
+            newBox.style.height="10rem";
+            newBox.style.width="10rem";
+            boxContainer.appendChild(newBox);
         }
     }
-    arrBoard = [1,2,3,4,5,6,7,8,9];
-    return {board: createBoard,};
+
+    function playerBanner (){
+        let playerBanner = document.createElement('div');
+        container.appendChild(playerBanner);
+    }
+    
+    //arrBoard = [1,2,3,4,5,6,7,8,9];
+    return {createBoard};
 })();
 
-const gameControl = (function(){
+/*const gameControl = (function(){
     let winMatrix = [
         [1,2,3],
         [4,5,6],
@@ -53,23 +68,20 @@ const gameControl = (function(){
 
     const p1 = playerOne();
     const p2 = playerTwo();
-    p1.setUser();
-    p2.setUser();
+    //p1.setUser();
+    //p2.setUser();
 
 
     for(let i=0;i<9;i++){
         
         if(i%2==0){
-            console.log(`${p1.uid}'s turn:`);
+           /* console.log(`${p1.uid}'s turn:`);
             let choice = parseInt(prompt("Choose a block P1:"));
             console.log(choice);
             p1.arr.push(choice);
             p1.arr.sort((a, b) => a - b);
             console.log(typeof(p1.arr[i]));
 
-            /*let isArrayInMatrix = winMatrix.some(
-                subArray => subArray.length === p1.arr.length && subArray.every((value, index) => value === p1.arr[index])
-            );*/
             let isArrayInMatrix = winMatrix.some(
                 subArray => subArray.every(value => p1.arr.includes(value))
             );
@@ -105,12 +117,17 @@ const gameControl = (function(){
             }
         }
     }
-})();
+})();*/
 
-const game = (function() {
+const game = (function() {;
+
     const start = document.querySelector('.start');
-    start.addEventListener('click',()=>{
-        
-    })
-    
+    function startgame(){
+        console.log('starting..')
+        document.querySelector('.welcome').remove();
+        document.querySelector('.start').remove();
+        gameboard.createBoard();
+    }
+
+    start.addEventListener('click', startgame);
 })();
