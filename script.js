0fdffda0cf816c11d23bea5aa7f9fadbdd6a0c32
k4rtikay <katt1bh4i@gmail.com>
2024-12-14 18:14:34 +0000

    Turns the console based logic to a dom based logic and implements the basic win con

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 const container = document.querySelector('.container');
-
-/*function playerOne(){
+const dialog = document.querySelector('dialog');
+const playerForm = document.querySelector('form');
+let flag = 0;
+function playerOne(){
     let uid;
     let arrP1 = [];
-    function user (){
-        uid = prompt("Enter Player One:");
+    function user (val){
+        uid = val;
     }
     return {setUser: user,
         get uid(){
@@ -18,8 +20,8 @@ const container = document.querySelector('.container');
 function playerTwo(){
     let uid;
     let arrP2 = [];
-    function user (){
-        uid = prompt("Enter Player Two:");
+    function user (val){
+        uid = val;
     }
     return {setUser: user,
         get uid(){
@@ -27,107 +29,153 @@ function playerTwo(){
         },
         arr: arrP2
     };
-}*/
+}
 
 
 
 
 const gameboard =(function () {
-    const boxContainer = document.querySelector('.boxContainer');
+
+    const p1 = playerOne();
+    const p2 = playerTwo();
+    
     function createBoard (){
-        for(let i=0;i<9;i++){
+        const boxContainer = document.createElement('div');
+        boxContainer.classList.add('boxContainer');
+        container.appendChild(boxContainer);
+        for(let i=1;i<=9;i++){
             let newBox = document.createElement('div');
             newBox.classList.add('box');
             newBox.style.height="10rem";
             newBox.style.width="10rem";
+            newBox.setAttribute('data-index',`${i}`);
             boxContainer.appendChild(newBox);
         }
     }
 
     function playerBanner (){
         let playerBanner = document.createElement('div');
+        playerBanner.style.height='4rem';
+        playerBanner.style.width='21rem';
+        playerBanner.classList.add('playerBanner');
+        playerBanner.textContent=`${p1.uid}'s turn`;
         container.appendChild(playerBanner);
+    }
+
+    function clearBoard(){
+        p1.arr='';
+        p2.arr='';
+        document.querySelector('.boxContainer').remove();
+        createBoard();
     }
     
     //arrBoard = [1,2,3,4,5,6,7,8,9];
-    return {createBoard};
+    return {createBoard, playerBanner, clearBoard};
 })();
 
-/*const gameControl = (function(){
-    let winMatrix = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7]
-    ];
-
-
-    const p1 = playerOne();
-    const p2 = playerTwo();
-    //p1.setUser();
-    //p2.setUser();
-
-
-    for(let i=0;i<9;i++){
-        
-        if(i%2==0){
-           /* console.log(`${p1.uid}'s turn:`);
-            let choice = parseInt(prompt("Choose a block P1:"));
-            console.log(choice);
-            p1.arr.push(choice);
-            p1.arr.sort((a, b) => a - b);
-            console.log(typeof(p1.arr[i]));
-
-            let isArrayInMatrix = winMatrix.some(
-                subArray => subArray.every(value => p1.arr.includes(value))
-            );
-            console.log(p1.arr);
-            console.log(isArrayInMatrix);
-            if(isArrayInMatrix && p1.arr.length>=3){
-                console.log(`${p1.uid} WINS!!!!`);
-                break;
-            }
-            if(i===8 && isArrayInMatrix==false){
-                console.log("IT'S A DRAW!!!!");
-                break;
-            }
-        }else{
-            console.log(`${p2.uid}'s turn:`);
-            let choice = parseInt(prompt("Choose a block P2:"));
-            console.log(choice);
-            p2.arr.push(choice);
-            p2.arr.sort((a, b) => a - b);
-            
-            let isArrayInMatrix = winMatrix.some(
-                subArray => subArray.every(value => p2.arr.includes(value))
-            );
-            console.log(isArrayInMatrix);
-            if(isArrayInMatrix&&p2.arr.length>=3){
-                console.log(`${p2.uid} WINS!!!!`);
-                break;
-            }
-
-            if(i===8 && isArrayInMatrix==false){
-                console.log("IT'S A DRAW!!!!");
-                break;
-            }
-        }
-    }
-})();*/
 
 const game = (function() {;
 
     const start = document.querySelector('.start');
     function startgame(){
-        console.log('starting..')
+        console.log('starting..');
+        document.querySelector('.modal').showModal();
         document.querySelector('.welcome').remove();
         document.querySelector('.start').remove();
         gameboard.createBoard();
+        gameboard.playerBanner();
+        
     }
 
     start.addEventListener('click', startgame);
 })();
+
+
+document.getElementById('playerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  })
+
+/*document.addEventListener("click", e => {
+    
+    if(!dialog.open) return;
+    const dialogDimensions = dialog.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      dialog.close();
+    }
+  })*/  
+
+document.querySelector('.submit').addEventListener('click',()=>{
+    
+    const gameControl = (function(){
+        let winMatrix = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,7]
+        ];
+    
+    
+        const p1 = playerOne();
+        const p2 = playerTwo();
+        p1.setUser(nameOne.value);
+        p2.setUser(nameTwo.value);
+
+        const selectBox = document.querySelectorAll('.box');
+        
+        selectBox.forEach(element => {
+            element.addEventListener('click', ()=>{
+                if(flag==0){
+                    console.log('p1 turn')
+                    let num = parseInt(element.getAttribute('data-index'));
+                    p1.arr.push(num);
+                    element.textContent='X';
+
+                    let isArrayInMatrix = winMatrix.some(
+                        subArray => subArray.every(value => p1.arr.includes(value))
+                    );
+                    if(isArrayInMatrix){
+                        document.querySelector('.playerBanner').textContent=`${p1.uid} WINS!!`
+                        gameboard.clearBoard();
+                    }else{
+                    console.log(p1.arr)
+                    flag=1;
+                    document.querySelector('.playerBanner').textContent=`${p2.uid}'s turn`;
+                    }
+                }else if(flag==1){
+                    console.log('p2 turn')
+                    let num = parseInt(element.getAttribute('data-index'));
+                    p2.arr.push(num);
+                    element.textContent='O';
+
+                    let isArrayInMatrix = winMatrix.some(
+                        subArray => subArray.every(value => p2.arr.includes(value))
+                    );
+                    if(isArrayInMatrix){
+                        document.querySelector('.playerBanner').textContent=`${p2.uid} WINS!!`
+                        gameboard.clearBoard();   
+                    }else{
+                    console.log(p2.arr)
+                    flag=0;
+                    document.querySelector('.playerBanner').textContent=`${p1.uid}'s turn`;
+                    }
+                    
+                }
+            })
+        });
+        
+        
+    
+    })();
+    
+    dialog.close();
+})
+
